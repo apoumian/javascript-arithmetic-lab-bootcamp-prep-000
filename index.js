777b90a0c2bd8c193920fa5d4748d6fbc91bea08
Arturo Poumian <apoumians@gmail.com>
2018-02-11 18:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-  return(a - b);
+  return a - b;
 }
 
 function multiply(a, b) {
-  return(a * b);
+  return a * b;
 }
 
 function divide(a, b) {
-  return(a / b);
+  return a / b;
 }
 
 function inc(a) {
-  return(a += 1);
+  return a += 1;
 }
 
 function dec(a) {
-  return(a -= 1);
+  return a -= 1;
 }
 
 function makeInt(n) {
